a04e59316a7624888ad90303cb2a052d3bd03551
Tyler <26290074+tylersayshi@users.noreply.github.com>
2025-10-19 16:46:51 -0700

    fix ui tests

--- packages/site/tests/setup.ts
+++ packages/site/tests/setup.ts
@@ -22,3 +22,17 @@ global.fetch = vi.fn(
 (global as any).MonacoEnvironment = {
 	getWorker: () => ({}) as any,
 };
+
+Object.defineProperty(window, "matchMedia", {
+	writable: true,
+	value: vi.fn().mockImplementation((query) => ({
+		matches: false,
+		media: query,
+		onchange: null,
+		addListener: vi.fn(),
+		removeListener: vi.fn(),
+		addEventListener: vi.fn(),
+		removeEventListener: vi.fn(),
+		dispatchEvent: vi.fn(),
+	})),
+});
